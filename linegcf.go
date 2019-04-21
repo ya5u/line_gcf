ac831d7ccd57a565ebfe93984633e62b827eb77d
Yasutaka Shinzaki <shinzaki@yasu26.tech>
2019-04-22 00:47:46 +0900

    feat: implement signiture verification

--- linegcf.go
+++ linegcf.go
@@ -1,9 +1,14 @@
 package linegcf
 
 import (
+	"bytes"
 	"context"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -11,11 +16,10 @@ import (
 	"cloud.google.com/go/firestore"
 )
 
-var projectID string = os.Getenv("GCP_PROJECT")
-
 var (
-	ctx context.Context
-	client *firestore.Client
+	projectID string = os.Getenv("GCP_PROJECT")
+	ctx       context.Context
+	client    *firestore.Client
 )
 
 func init() {
@@ -30,43 +34,49 @@ func init() {
 	}
 }
 
-var req struct {
-	Destination string `json: "destination"`
-	Events      []Event
-}
+var (
+	lineChannelSecret string = os.Getenv("LINE_CHANNEL_SECRET")
+	req               struct {
+		Destination string `json:"destination"`
+		Events      []Event
+	}
+)
 
 // Event Object
 type Event struct {
-	Type       string `json: "type"`
-	Timestamp  int64  `json: "timestamp"`
-	ReplyToken string `json: "replyToken"`
-	Source     struct {
-		Type    string `json: "type"`
-		UserID  string `json: "userId"`
-		GroupID string `json: "groupId"`
-		RoomID  string `json: roomId`
-	}
-	Message LineMessage
+	Type       string `json:"type"`
+	Timestamp  int64  `json:"timestamp"`
+	ReplyToken string `json:"replyToken"`
+	Source     LineSource
+	Message    LineMessage
+}
+
+// LineSource Object
+type LineSource struct {
+	Type    string `json:"type"`
+	UserID  string `json:"userId"`
+	GroupID string `json:"groupId"`
+	RoomID  string `json:"roomId"`
 }
 
 // LineMessage Object
 type LineMessage struct {
-	ID              string  `json: "id"`
-	Type            string  `json: "type"`
-	Text            string  `json: "text"`
-	PackageID       string  `json: "packageId"`
-	StickerID       string  `json: "stickerId"`
-	FileName        string  `json: "fileName"`
-	FileSize        int64   `json: "fileSize"`
-	Title           string  `json: "title"`
-	Address         string  `json: "address"`
-	Latitude        float64 `json: "latitude"`
-	Longitude       float64 `json: "longitude"`
-	Duration        int64   `json: "duration"`
+	ID              string  `json:"id"`
+	Type            string  `json:"type"`
+	Text            string  `json:"text"`
+	PackageID       string  `json:"packageId"`
+	StickerID       string  `json:"stickerId"`
+	FileName        string  `json:"fileName"`
+	FileSize        int64   `json:"fileSize"`
+	Title           string  `json:"title"`
+	Address         string  `json:"address"`
+	Latitude        float64 `json:"latitude"`
+	Longitude       float64 `json:"longitude"`
+	Duration        int64   `json:"duration"`
 	ContentProvider struct {
-		Type               string `json: "type"`
-		OriginalContentURL string `json: "originalContentUrl"`
-		PreviewImageURL    string `json: "previewImageUrl"`
+		Type               string `json:"type"`
+		OriginalContentURL string `json:"originalContentUrl"`
+		PreviewImageURL    string `json:"previewImageUrl"`
 	}
 }
 
@@ -74,9 +84,22 @@ type LineMessage struct {
 func Handler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Content-Type: %s", r.Header.Get("Content-Type"))
 
+	defer r.Body.Close()
+	var bodyBytes []byte
+	bodyBytes, _ = ioutil.ReadAll(r.Body)
+
+	verified := verifySignature(r.Header.Get("X-Line-Signature"), bodyBytes)
+	if !verified {
+		log.Printf("Signature is NOT Verified")
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+
+	r.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
 		log.Printf("JSON parse ERROR: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
@@ -114,37 +137,59 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 			log.Printf("Type: %s", e.Message.Type)
 		}
 
-		go writeMsg(e.Timestamp, e.Message)
+		go writeMsg(e.Timestamp, e.ReplyToken, e.Source, e.Message)
 	}
 
 	fmt.Fprintf(w, "Ok")
 }
 
-// FsMessage Document in Firestore collection "Messages"
-type FsMessage struct {
-	Timestamp          int64   `firestore: "timestamp"`
-	Type               string  `firestore: "type"`
-	Text               string  `firestore: "text"`
-	PackageID          string  `firestore: "packageId"`
-	StickerID          string  `firestore: "stickerId"`
-	FileName           string  `firestore: "fileName"`
-	FileSize           int64   `firestore: "fileSize"`
-	Title              string  `firestore: "title"`
-	Address            string  `firestore: "address"`
-	Latitude           float64 `firestore: "latitude"`
-	Longitude          float64 `firestore: "longitude"`
-	Duration           int64   `firestore: "duration"`
-	ContentType        string  `firestore: "contentType"`
-	OriginalContentURL string  `firestore: "originalContentUrl"`
-	PreviewImageURL    string  `firestore: "previewImageUrl"`
+func verifySignature(signature string, body []byte) bool {
+	decoded, err := base64.StdEncoding.DecodeString(signature)
+	if err != nil {
+		log.Printf("Base64 decode ERROR: %v", err)
+		return false
+	}
+
+	hash := hmac.New(sha256.New, []byte(lineChannelSecret))
+	hash.Write(body)
+	return hmac.Equal(decoded, hash.Sum(nil))
 }
 
-func writeMsg(timestamp int64, msg LineMessage) {
-	log.Printf("writeMsg - timestamp:%d msg:%v", timestamp, msg)
+// FsMessage Document in Firestore collection "Messages"
+type FsMessage struct {
+	Timestamp          int64   `firestore:"timestamp"`
+	ReplyToken         string  `firestore:"replyToken"`
+	SourceType         string  `firestore:"sourceType"`
+	UserID             string  `firestore:"userID"`
+	GroupID            string  `firestore:"groupID"`
+	RoomID             string  `firestore:"roomID"`
+	Type               string  `firestore:"type"`
+	Text               string  `firestore:"text"`
+	PackageID          string  `firestore:"packageID"`
+	StickerID          string  `firestore:"stickerID"`
+	FileName           string  `firestore:"fileName"`
+	FileSize           int64   `firestore:"fileSize"`
+	Title              string  `firestore:"title"`
+	Address            string  `firestore:"address"`
+	Latitude           float64 `firestore:"latitude"`
+	Longitude          float64 `firestore:"longitude"`
+	Duration           int64   `firestore:"duration"`
+	ContentType        string  `firestore:"contentType"`
+	OriginalContentURL string  `firestore:"originalContentURL"`
+	PreviewImageURL    string  `firestore:"previewImageURL"`
+}
+
+func writeMsg(timestamp int64, replyToken string, source LineSource, msg LineMessage) {
+	log.Printf("writeMsg - timestamp:%d source:%v msg:%v", timestamp, source, msg)
 
 	doc := client.Doc("Messages/" + msg.ID)
-	wr, err := doc.Create(ctx, FsMessage {
+	wr, err := doc.Create(ctx, FsMessage{
 		Timestamp:          timestamp,
+		ReplyToken:         replyToken,
+		SourceType:         source.Type,
+		UserID:             source.UserID,
+		GroupID:            source.GroupID,
+		RoomID:             source.RoomID,
 		Type:               msg.Type,
 		Text:               msg.Text,
 		PackageID:          msg.PackageID,
--- linegcf_test.go
+++ linegcf_test.go
@@ -1,6 +1,7 @@
 package linegcf
 
 import (
+	"log"
 	"reflect"
 	"testing"
 )
@@ -9,51 +10,62 @@ func TestWriteMsg(t *testing.T) {
 	testID := "testID"
 	var (
 		testTimestamp int64 = 123456789
-		testFileSize int64 = 12345
-		testDuration int64 = 12345
+		testFileSize  int64 = 12345
+		testDuration  int64 = 12345
 	)
-	var testData = LineMessage {
-		ID:              testID,
-		Type:            "test Type",
-		Text:            "test Text",
-		PackageID:       "test PackageID",
-		StickerID:       "test StickerID",
-		FileName:        "test FileName",
-		FileSize:        testFileSize,
-		Title:           "test Title",
-		Address:         "test Address",
-		Latitude:        1234.5,
-		Longitude:       1234.5,
-		Duration:        testDuration,
+	var testData = LineMessage{
+		ID:        testID,
+		Type:      "test Type",
+		Text:      "test Text",
+		PackageID: "test PackageID",
+		StickerID: "test StickerID",
+		FileName:  "test FileName",
+		FileSize:  testFileSize,
+		Title:     "test Title",
+		Address:   "test Address",
+		Latitude:  1234.5,
+		Longitude: 1234.5,
+		Duration:  testDuration,
 		ContentProvider: struct {
-			Type               string `json: "type"`
-			OriginalContentURL string `json: "originalContentUrl"`
-			PreviewImageURL    string `json: "previewImageUrl"`
+			Type               string `json:"type"`
+			OriginalContentURL string `json:"originalContentUrl"`
+			PreviewImageURL    string `json:"previewImageUrl"`
 		}{
 			Type:               "test ContentType",
 			OriginalContentURL: "test OriginalContentURL",
 			PreviewImageURL:    "test PreviewImageURL",
 		},
 	}
-	expected := map[string]interface{} {
-		"Timestamp":          testTimestamp,
-		"Type":               "test Type",
-		"Text":               "test Text",
-		"PackageID":          "test PackageID",
-		"StickerID":          "test StickerID",
-		"FileName":           "test FileName",
-		"FileSize":           testFileSize,
-		"Title":              "test Title",
-		"Address":            "test Address",
-		"Latitude":           1234.5,
-		"Longitude":          1234.5,
-		"Duration":           testDuration,
-		"ContentType":        "test ContentType",
-		"OriginalContentURL": "test OriginalContentURL",
-		"PreviewImageURL":    "test PreviewImageURL",
+	var testSource = LineSource{
+		Type:    "test SourceType",
+		UserID:  "test UserID",
+		GroupID: "test GroupID",
+		RoomID:  "test RoomID",
+	}
+	expected := map[string]interface{}{
+		"timestamp":          testTimestamp,
+		"replyToken":         "test ReplyToken",
+		"sourceType":         "test SourceType",
+		"userID":             "test UserID",
+		"groupID":            "test GroupID",
+		"roomID":             "test RoomID",
+		"type":               "test Type",
+		"text":               "test Text",
+		"packageID":          "test PackageID",
+		"stickerID":          "test StickerID",
+		"fileName":           "test FileName",
+		"fileSize":           testFileSize,
+		"title":              "test Title",
+		"address":            "test Address",
+		"latitude":           1234.5,
+		"longitude":          1234.5,
+		"duration":           testDuration,
+		"contentType":        "test ContentType",
+		"originalContentURL": "test OriginalContentURL",
+		"previewImageURL":    "test PreviewImageURL",
 	}
 
-	writeMsg(testTimestamp, testData)
+	writeMsg(testTimestamp, "test ReplyToken", testSource, testData)
 
 	docRef := client.Doc("Messages/" + testID)
 	docsnap, err := docRef.Get(ctx)
@@ -62,10 +74,15 @@ func TestWriteMsg(t *testing.T) {
 	}
 	r := docsnap.Data()
 
+	log.Printf("result:%v", r["sourceType"])
+	log.Printf("expected:%v", expected["sourceType"])
+	log.Printf("result:%v", r["userID"])
+	log.Printf("expected:%v", expected["userID"])
+	log.Printf("result:%v", r["groupID"])
+	log.Printf("expected:%v", expected["groupID"])
+
+	defer docRef.Delete(ctx)
 	if !reflect.DeepEqual(r, expected) {
-		docRef.Delete(ctx)
 		t.Errorf("WriteResult does not deep equal expected: %v", r)
 	}
-
-	docRef.Delete(ctx)
 }
